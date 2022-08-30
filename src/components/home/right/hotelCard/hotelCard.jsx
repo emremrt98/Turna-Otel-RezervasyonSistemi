1be7cd26b20e34fe04f7b1436adc2eb1899613aa
Emre Mert <emre.mrt98@gmail.com>
2022-08-30 17:27:06 +0300

    turna v6 created card component

--- src/components/home/right/hotelCard/hotelCard.jsx
+++ src/components/home/right/hotelCard/hotelCard.jsx
@@ -1,29 +1,32 @@
-import Room1 from '../../../../assets/rooms/room1.jpg';
 import './hotelCard.scss';
 import { AiOutlineInfoCircle, AiOutlineCheckCircle } from 'react-icons/ai';
-
 import { FaArrowRight } from 'react-icons/fa';
-function HotelCard() {
+
+function HotelCard({ hotelRoomInfo: { name, img, advantage: [adv1, adv2, adv3], property, price, opportunityPrice } }) {
+
     return (
         <section className='hotelCard'>
-            <figure className='hotelCard__img'><img src={Room1} alt="room1" /></figure>
+            <figure className='hotelCard__img'><img src={img} alt="room1" /></figure>
             <article className='hotelCard__generalInfo'>
                 <div className='hotelCard__Info'>
-                    <h5 className='hotelCard__roomType'>Standart Oda</h5>
+                    <h5 className='hotelCard__roomType'>{name}</h5>
                     <div className='hotelCard__advantage'>
-                        <span >Erken Rezervasyon İndirimi</span>
-                        <span >Ücretsiz İptal <AiOutlineInfoCircle className='hotelCard__icon' /></span>
-                        <span >Her Şey Dahil</span>
+                        <span >{adv1}</span>
+                        <span >{adv2}<AiOutlineInfoCircle className='hotelCard__icon' /></span>
+                        <span >{adv3}</span>
                     </div>
                     <div className='hotelCard__properties'>
                         <div className='hotelCard__property'>
-                            <p><AiOutlineCheckCircle className='hotelCard__icons' />Banyo</p>
-                            <p><AiOutlineCheckCircle className='hotelCard__icons' />İnternet</p>
-                            <p><AiOutlineCheckCircle className='hotelCard__icons' />Havlu Seti</p>
+                            {
+                                property.map((data,index)=>(
+                                    <p key={index}><AiOutlineCheckCircle className='hotelCard__icons' />{property[index]}</p>
+                                ))
+                            }
+                            
                         </div>
                         <div className="hotelCard__price">
-                            <p>₺1.500</p>
-                            <p>₺1.200</p>
+                            <p>₺{price}</p>
+                            <p>₺{opportunityPrice}</p>
                         </div>
                     </div>
                 </div>
--- src/components/home/right/rightSide.jsx
+++ src/components/home/right/rightSide.jsx
@@ -4,7 +4,28 @@ import { AiFillStar } from 'react-icons/ai';
 import { FaMapMarker } from 'react-icons/fa';
 import Carousel from './carousel/carousel';
 import HotelCard from './hotelCard/hotelCard';
+import Room1 from '../../../assets/rooms/room1.jpg';
+import Room2 from '../../../assets/rooms/room2.jpg';
 function RightSide() {
+    const hotelRoomInfo = [
+        {
+            name: "Standart Oda",
+            img: Room1,
+            advantage: ["Erken Rezervasyon İndirimi", "Ücretsiz İptal", "Her Şey Dahil"],
+            property: ["Banyo", "İnternet", "Havlu Seti"],
+            price: "1.500",
+            opportunityPrice: "1.200",
+        },
+        {
+            name: "İki Balkonlu Oda (Delux)",
+            img: Room2,
+            advantage: ["Erken Rezervasyon İndirimi", "Ücretsiz İptal", "Her Şey Dahil"],
+            property: ["Banyo", "İnternet", "Havlu Seti"],
+            price: "2.000",
+            opportunityPrice: "1.700",
+        }
+    ]
+    console.log(hotelRoomInfo[0].property);
     return (
         <section className='RightSide'>
             <div className='rigtSide__head'>
@@ -19,9 +40,9 @@ function RightSide() {
                 <p className='rightSide__info'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio, illo exercitationem doloribus mollitia dicta ipsa provident, numquam officiis harum quas expedita, incidunt esse voluptates sequi iure sapiente voluptatibus molestiae ad minima eos tempore molestias aspernatur dolor recusandae! </p>
             </figure>
             <article className='rightSide__card'>
-                <HotelCard />
+                <HotelCard hotelRoomInfo={hotelRoomInfo[0]} />
                 <br />
-                <HotelCard />
+                <HotelCard hotelRoomInfo={hotelRoomInfo[1]}/>
             </article>
         </section>
     )
